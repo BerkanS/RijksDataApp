742f4fc99e12919421364792f6a769347316e43c
BerkanS <serbest_berkan00@hotmail.com>
2022-09-25 22:00:07 +0200

    Finished repository testing

--- app/src/androidTest/java/com/berkan/rijksdataapp/domain/model/Fakes.kt
+++ app/src/androidTest/java/com/berkan/rijksdataapp/domain/model/Fakes.kt
@@ -4,7 +4,7 @@ object Fakes {
     val artObject = ArtObject(
         objectNumber = "OBJ-001",
         title = "Test Object",
-        author = "Rembrandt",
+        author = "Vermeer",
         longTitle = "Test Object Long",
         webImage = ObjectImage("test"),
         headerImage = ObjectImage("test")
@@ -15,6 +15,42 @@ object Fakes {
         title = "Test Object 2",
         author = "Rembrandt",
         longTitle = "Test Object 2 Long",
+        webImage = ObjectImage("test"),
+        headerImage = ObjectImage("test")
+    )
+
+    val artObjectThree = ArtObject(
+        objectNumber = "OBJ-003",
+        title = "Test Object 3",
+        author = "Rembrandt",
+        longTitle = "Test Object 3 Long",
+        webImage = ObjectImage("test"),
+        headerImage = ObjectImage("test")
+    )
+
+    val artObjectFour = ArtObject(
+        objectNumber = "OBJ-004",
+        title = "Test Object 4",
+        author = "Rembrandt",
+        longTitle = "Test Object 4 Long",
+        webImage = ObjectImage("test"),
+        headerImage = ObjectImage("test")
+    )
+
+    val artObjectFive = ArtObject(
+        objectNumber = "OBJ-005",
+        title = "Test Object 5",
+        author = "Vermeer",
+        longTitle = "Test Object 5 Long",
+        webImage = ObjectImage("test"),
+        headerImage = ObjectImage("test")
+    )
+
+    val artObjectSix = ArtObject(
+        objectNumber = "OBJ-006",
+        title = "Test Object 6",
+        author = "Van Gogh",
+        longTitle = "Test Object 6 Long",
         webImage = ObjectImage("test"),
         headerImage = ObjectImage("test")
     )
